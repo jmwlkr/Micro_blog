8d71cc4f7129342bbbf0f553e767381b03bc71a3
poopoothegorilla <jm.wlkr1@gmail.com>
2014-11-08 23:15:32 -0500

    create layout file sitewide css and make nav

--- app.rb
+++ app.rb
@@ -8,19 +8,26 @@ require 'sinatra/flash' # require 'bundler'
 set :database, 'sqlite3:formdemo.sqlite3'
 enable :sessions
 
-get "/" do
+def current_user
   if session[:user_id].nil?
     @user = nil
   else
     @user = User.find(session[:user_id])
   end
+end
 
+get "/" do
+  @user = current_user
   @posts = Post.all
 
   # @followers = @user.followers
   # @following = @user.leaders
 
   erb :index
+end
+
+get "/posts" do
+
 end
 
 get "/signup" do
@@ -30,7 +37,7 @@ end
 post "/signup" do
   puts params.inspect
   if User.where(email: params[:user][:email]).first
-    flash[:alert] = "You Stink"
+    flash[:alert] = "That Email is taken!"
     redirect "/"
   end
   @user = User.new(params[:user])
@@ -47,14 +54,14 @@ post "/login" do
   @user = User.where(email: params[:email]).first
   if @user && @user.password == params[:password]
     session[:user_id] = @user.id
-    flash[:notice] = "Great You got in!"
+    flash[:notice] = "Logged In Successful!"
   else
     flash[:alert] = "Invalid Attempt"
   end
   redirect "/"
 end
 
-get "/logout" do 
+get "/logout" do
   session[:user_id] = nil
   redirect "/"
 end
@@ -64,7 +71,7 @@ get "/post/new" do
 end
 
 post "/post/new" do
-  @post = Post.new(text: params[:text], 
+  @post = Post.new(text: params[:text],
                    user_id: session[:user_id])
   if @post.save
     flash[:notice] = "Post was Successfully Created"
@@ -87,6 +94,3 @@ get "/post/:potato/delete" do
   end
   redirect "/"
 end
-
-
-
