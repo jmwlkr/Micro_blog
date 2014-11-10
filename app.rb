52d4de44ff3b3331b13af1187d56df2cf8ed1990
poopoothegorilla <jm.wlkr1@gmail.com>
2014-11-09 23:15:47 -0500

    add follow ability

--- app.rb
+++ app.rb
@@ -9,15 +9,17 @@ enable :sessions
 # This method is accessible in all of our erb files!! COOL RIGHT!
 def current_user
   if session[:user_id].nil?
-    return nil
+    @current_user = nil
   else
-    return User.find(session[:user_id])
+    @current_user =  User.find(session[:user_id])
   end
+  return @current_user
 end
 
 #### INDEX ROUTE ####
 
 get "/" do
+  current_user
   @posts = Post.all
 
   # @followers = @user.followers
@@ -29,6 +31,7 @@ end
 #### SIGNUP ####
 
 get "/signup" do
+  current_user
   erb :signup
 end
 
@@ -74,44 +77,88 @@ end
 
 get "/user/:id" do
   @user = User.find(params[:id])
+  current_user
   @profile = @user.profile
   @posts = @user.posts
+  @follow = Follower.where(follower_id: session[:user_id],
+                           leader_id: params[:id]).first
+
   erb :user
 end
 
 post "/profile/new" do
+  current_user
 
-  if current_user && !current_user.profile.nil?
-    @profile = current_user.profile
+  if @current_user && !@current_user.profile.nil?
+    @profile = @current_user.profile
 
     if @profile.update(params[:profile])
       flash[:notice] = "Profile Updated"
     else
       flash[:alert] = "The Profile was not updated!"
     end
-  elsif current_user && !current_user.profile
-    @profile = Profile.new(bio: params[:profile][:bio], user_id: session[:user_id])
+
+  elsif @current_user && !@current_user.profile
+
+    @profile = Profile.new(bio: params[:profile][:bio],
+                           user_id: session[:user_id])
     if @profile.save
       flash[:notice] = "Profile Created"
     else
       flash[:alert] = "The Profile was not Created!"
     end
+
   else
     flash[:alert] = "Something went terribly wrong"
   end
 
-  redirect "/user/#{ current_user.id }"
+  redirect "/user/#{ @current_user.id }"
+end
+
+#### FOLLOWERS ROUTS ####
+
+get "/user/:id/follow" do
+  @follow = Follower.new(follower_id: session[:user_id], leader_id: params[:id])
+  if @follow.save
+    flash[:notice] = "You are now following this User"
+  else
+    flash[:alert] = "Something went wrong!"
+  end
+
+  redirect "/user/#{params[:id]}"
+end
+
+get "/follow/:id/delete" do
+  @follow = Follower.find(params[:id])
+
+
+  if @follow.follower_id != session[:user_id]
+    flash[:alert] = "YOU ARE NOT THAT USER"
+  else
+
+    if @follow.destroy
+      flash[:notice] = "You are no longer following this User"
+    else
+      flash[:alert] = "Something went wrong!"
+    end
+
+  end
+
+  redirect back
 end
 
 #### POSTS ROUTES ####
 
 get "/posts" do
+  current_user
   @posts = Post.all
 
   erb :posts
 end
 
 get "/post/new" do
+  current_user
+
   erb :new_post
 end
 
@@ -121,6 +168,8 @@ post "/post/new" do
 
   if @post.save
     flash[:notice] = "Post was Successfully Created"
+  else
+    flash[:alert] = "Something went wrong!"
   end
 
   redirect "/"
@@ -134,6 +183,8 @@ get "/post/:id/delete" do
   else
     if @post.destroy
       flash[:notice] = "THE POST WAS DESTROYED"
+    else
+      flash[:alert] = "Something went wrong!"
     end
   end
 
